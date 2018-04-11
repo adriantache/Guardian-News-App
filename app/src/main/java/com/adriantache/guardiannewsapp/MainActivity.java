f1010ffbc2b0f9a92e2166c756e507303dd4c120
Adrian Tache <contact@adriantache.com>
2018-04-11 20:37:54 +0300

    Finalize API key protection code

--- app/src/main/java/com/adriantache/guardiannewsapp/MainActivity.java
+++ app/src/main/java/com/adriantache/guardiannewsapp/MainActivity.java
@@ -6,17 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String GUARDIAN_API_KEY;
+    private String GUARDIAN_API_KEY = "test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        readAPIKey();
+        //read Guardian API key from file so we don't just post it to GitHub
+        //if file is not present just use `test` API key
+        try {
+            if (Arrays.asList(getResources().getAssets().list("")).contains("myFile")) readAPIKey();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void readAPIKey() {
@@ -32,8 +39,9 @@ public class MainActivity extends AppCompatActivity {
             int ch;
             StringBuilder sb = new StringBuilder();
             try {
-                while ((ch = inputStream.read()) != -1)
+                while ((ch = inputStream.read()) != -1) {
                     sb.append((char) ch);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
