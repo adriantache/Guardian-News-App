71dc684c051948c0d4b4a881e2d307d86829eced
Adrian Tache <contact@adriantache.com>
2018-04-12 10:43:32 +0300

    Set up NewsItem class

---
+++ app/src/main/java/com/adriantache/guardiannewsapp/NewsItem.java
@@ -0,0 +1,51 @@
+package com.adriantache.guardiannewsapp;
+
+import android.graphics.Bitmap;
+
+public class NewsItem {
+    private String category;
+    private String title;
+    private String url;
+    private String date;
+    private Bitmap thumbnail;
+
+    public NewsItem(String category, String title, String url, String date, Bitmap thumbnail) {
+        this.category = category;
+        this.title = title;
+        this.url = url;
+        this.date = cleanUpDateString(date);
+        this.thumbnail = thumbnail;
+    }
+
+    public NewsItem(String category, String author, String title, String url, String date, Bitmap thumbnail) {
+        this.category = category + " | by " + author;
+        this.title = title;
+        this.url = url;
+        this.date = cleanUpDateString(date);
+        this.thumbnail = thumbnail;
+    }
+
+    private String cleanUpDateString(String date) {
+        return date.replaceAll("T", "\n").replaceAll("Z", "");
+    }
+
+    public Bitmap getThumbnail() {
+        return thumbnail;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
