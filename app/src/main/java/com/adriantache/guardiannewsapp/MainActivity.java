d664e0bd5313aa44a0279312982449ae3ab52ced
Adrian Tache <contact@adriantache.com>
2018-04-17 16:30:44 +0300

    Finish menu implementation

--- app/src/main/java/com/adriantache/guardiannewsapp/MainActivity.java
+++ app/src/main/java/com/adriantache/guardiannewsapp/MainActivity.java
@@ -2,17 +2,18 @@ package com.adriantache.guardiannewsapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -43,23 +44,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Read Guardian API key from file so we don't just post it to GitHub.
-        //If file is not present just use `test` API key.
-        String GUARDIAN_API_KEY = "test";
-        try {
-            if (Arrays.asList(getResources().getAssets().list("")).contains("GuardianAPI.txt")) {
-                String temp = readAPIKey();
-                if (!TextUtils.isEmpty(temp)) GUARDIAN_API_KEY = temp;
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Cannot open API key file.", e);
-        }
-        //then create Guardian URL
-        GUARDIAN_URL =
-                "http://content.guardianapis.com/search?order-by=newest&tag=technology%2Fandroid" +
-                        "&section=technology&page-size=100&show-fields=thumbnail&api-key=" +
-                        GUARDIAN_API_KEY;
 
         //find views
         listView = findViewById(R.id.listView);
@@ -104,30 +88,50 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         });
     }
 
-    private String readAPIKey() {
-        AssetManager am = getApplicationContext().getAssets();
-        InputStream inputStream = null;
+    private void readAPIKey() {
+        //If file is not present just use `test` API key.
+        String GUARDIAN_API_KEY = "test";
+
+        //get API key from file
         try {
-            inputStream = am.open("GuardianAPI.txt");
-        } catch (IOException e) {
-            Log.e(TAG, "Cannot read API key from file.", e);
-        }
-
-        if (inputStream != null) {
-            int ch;
-            StringBuilder sb = new StringBuilder();
-            try {
-                while ((ch = inputStream.read()) != -1) {
-                    sb.append((char) ch);
+            if (Arrays.asList(getResources().getAssets().list("")).contains("GuardianAPI.txt")) {
+                AssetManager am = getApplicationContext().getAssets();
+                InputStream inputStream = null;
+                try {
+                    inputStream = am.open("GuardianAPI.txt");
+                } catch (IOException e) {
+                    Log.e(TAG, "Cannot read API key from file.", e);
                 }
-            } catch (IOException e) {
-                Log.e(TAG, "Cannot read API key InputStream.", e);
-            }
 
-            return sb.toString();
+                if (inputStream != null) {
+                    int ch;
+                    StringBuilder sb = new StringBuilder();
+                    try {
+                        while ((ch = inputStream.read()) != -1) {
+                            sb.append((char) ch);
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Cannot read API key InputStream.", e);
+                    }
+
+                    if (sb.length() != 0) GUARDIAN_API_KEY = sb.toString();
+                }
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Cannot open API key file.", e);
         }
 
-        return null;
+        //get user preferences
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // getString retrieves a String value from the preferences. The second parameter is the default value for this preference.
+        String number_of_posts = sharedPrefs.getString(
+                getString(R.string.posts_to_fetch_key), getString(R.string.posts_to_fetch_default));
+
+        //finally create Guardian URL
+        GUARDIAN_URL = "http://content.guardianapis.com/search?order-by=newest&tag=technology%2Fandroid" +
+                "&section=technology&page-size=" + number_of_posts +
+                "&show-fields=thumbnail&api-key=" + GUARDIAN_API_KEY;
     }
 
     private void hideProgressBar() {
@@ -148,6 +152,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @NonNull
     @Override
     public Loader<List<NewsItem>> onCreateLoader(int id, @Nullable Bundle args) {
+        //Read Guardian API key from file so we don't just post it to GitHub
+        readAPIKey();
+
         return new NewsLoader(this, GUARDIAN_URL);
     }
 
@@ -168,7 +175,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     //menu code
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
--- app/src/main/java/com/adriantache/guardiannewsapp/SettingsActivity.java
+++ app/src/main/java/com/adriantache/guardiannewsapp/SettingsActivity.java
@@ -1,9 +1,11 @@
 package com.adriantache.guardiannewsapp;
 
-import android.preference.PreferenceFragment;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -13,13 +15,28 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+    public static class SettingsFragment extends PreferenceFragmentCompat implements android.support.v7.preference.Preference.OnPreferenceChangeListener {
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             addPreferencesFromResource(R.xml.preferences);
+            Preference number_of_posts = findPreference(getString(R.string.posts_to_fetch_key));
+            bindPreferenceSummaryToValue(number_of_posts);
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), getString(R.string.posts_to_fetch_default));
+            onPreferenceChange(preference, preferenceString);
         }
 
 
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
     }
 }
